177732ff100f90bfc0bc0ca2ff7f4f0f89b64ab7
Alex <tokarev_sanya2803_5@mail.ru>
2023-10-23 20:52:35 +0300

    Добавить HTML-шаблоны и стили

--- Spirit/main/urls.py
+++ Spirit/main/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.alex),
-    path('Артур', views.artur)
+    path('Alex', views.artur)
 ]
--- Spirit/main/views.py
+++ Spirit/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def alex(request):
-    return HttpResponse("<h4>Тимофей пидор</h4>")
+    return render(request, 'main/index.html')
+
 def artur(request):
-    return HttpResponse("<h4>Артур спидраннер</h4>")
+    return render(request, 'main/about.html')
